a3508df1d3849d3a0a196227758a7a6168df6732
Mihyro <127537760+Mihyro@users.noreply.github.com>
2023-11-06 14:14:54 +0700

    Add files via upload
    class computer (destop) thường, là class nhập và định dạng

---
+++ computer.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class computer {
+        // Class data members
+        public int No;
+        public String ComName;
+        public String BrandName;
+        public int Price;
+        public int ComQty;
+        public int ComQtyCopy;
+
+        Scanner input = new Scanner(System.in);
+
+        public computer() {
+                System.out.println("Nhập mã sản phẩm:");
+                this.No = input.nextInt();
+                input.nextLine();
+
+                System.out.println("Nhập tên sản phẩm:");
+                this.ComName = input.nextLine();
+
+                System.out.println("Nhập nhà sản xuất:");
+                this.BrandName = input.nextLine();
+
+                System.out.println("Nhập giá tiền:");
+                this.Price = input.nextInt();
+
+                System.out.println("Nhập số lượng:");
+                this.ComQty = input.nextInt();
+                ComQtyCopy = this.ComQty;
+        }
+}
