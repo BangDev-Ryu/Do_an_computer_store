c04945e11dd57d56309054b065c5f7564beacebf
Bang <ryushiki12@gmail.com>
2023-11-24 22:32:18 +0700

    checked

--- chiTietHoaDon.java
+++ chiTietHoaDon.java
@@ -1,53 +1,65 @@
 import java.util.Scanner;
+
 public class chiTietHoaDon {
+    Scanner sc = new Scanner(System.in);
+    
     private String idHoaDon;
     private String maSp;
     private int soLuong;
     private double giaTien;
-    Scanner sc = new Scanner(System.in);
-    public String getIdHoaDon() {
-        return idHoaDon;
+
+    public chiTietHoaDon() {
+
     }
-    public void setIdHoaDon(String idHoaDon) {
-        this.idHoaDon = idHoaDon;
-    }
-    public String getMaSp() {
-        return maSp;
-    }
-    public void setMaSp(String maSp) {
-        this.maSp = maSp;
-    }
-    public int getSoLuong() {
-        return soLuong;
-    }
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-    }
-    public double getGiaTien() {
-        return giaTien;
-    }
-    public void setGiaTien(double giaTien) {
-        this.giaTien = giaTien;
-    }
-    
+
     public chiTietHoaDon(String idHoaDon, String maSp, int soLuong, int giaTien) {
         this.idHoaDon = idHoaDon;
         this.maSp = maSp;
         this.soLuong = soLuong;
         this.giaTien = giaTien;
     }
-    public chiTietHoaDon() {
+    
+    public String getIdHoaDon() {
+        return idHoaDon;
     }
+    
+    public void setIdHoaDon(String idHoaDon) {
+        this.idHoaDon = idHoaDon;
+    }
+    
+    public String getMaSp() {
+        return maSp;
+    }
+    
+    public void setMaSp(String maSp) {
+        this.maSp = maSp;
+    }
+    
+    public int getSoLuong() {
+        return soLuong;
+    }
+    
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+    
+    public double getGiaTien() {
+        return giaTien;
+    }
+    
+    public void setGiaTien(double giaTien) {
+        this.giaTien = giaTien;
+    }
+    
     public void nhapCT(){
         System.out.println("Nhap ma san pham: ");
         maSp = sc.nextLine();
         System.out.println("Nhap so luong san pham");
         soLuong = sc.nextInt();
     }
-    @Override
+
     public String toString() {
         return "chiTietHoaDon [idHoaDon=" + idHoaDon + ", maSp=" + maSp + ", soLuong=" + soLuong + ", giaTien="
                 + giaTien + "]";
     }
-    
 }
