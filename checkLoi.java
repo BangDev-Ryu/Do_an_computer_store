7f391204ca54c69eb4380fb7a6a6089ce5d3a32e
Bang <ryushiki12@gmail.com>
2023-12-01 10:37:23 +0700

    update class check loi

--- checkLoi.java
+++ checkLoi.java
@@ -34,26 +34,107 @@ public class checkLoi {
     }
 
     public static String checkIdNhaCungCap() {
-
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            input = input.toUpperCase();
+            if (input.matches("CC"+"[0-9]{3}")) {
+                return input;
+            }
+            else {
+                System.out.println("ID sai dinh dang CC___. Vi du: CC001");
+            }
+            System.out.println("Moi nhap lai: ");
+        }
     }
 
     public static String checkIdHoaDon() {
-
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            input = input.toUpperCase();
+            if (input.matches("HD"+"[0-9]{3}")) {
+                return input;
+            }
+            else {
+                System.out.println("ID sai dinh dang HD___. Vi du: HD001");
+            }
+            System.out.println("Moi nhap lai: ");
+        }
     }
 
     public static String checkIdPhieuNhap() {
-
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            input = input.toUpperCase();
+            if (input.matches("PN"+"[0-9]{3}")) {
+                return input;
+            }
+            else {
+                System.out.println("ID sai dinh dang PN___. Vi du: PN001");
+            }
+            System.out.println("Moi nhap lai: ");
+        }
     }
 
     public static String checkTenKhachHang() {
-
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (input.matches("[\\pL\\pMn*\\s*]+")) {
+                input = input.trim();
+                input = input.replaceAll("\\s+", " ");
+                input = input.toLowerCase();
+                String[] Chuoi = input.split(" ");
+                input = "";
+                for (int i = 0; i < Chuoi.length; i++) {
+                    input += String.valueOf(Chuoi[i].charAt(0)).toUpperCase() + Chuoi[i].substring(1);
+                    if (i < Chuoi.length - 1) {
+                        input += " ";
+                    }
+                }
+                return input;
+            } else {
+                System.out.print("Ten khach hang khong hop le.");
+            }
+            System.out.println("Moi nhap lai: ");
+        }
     }
 
     public static String checkTenNhaCungCap() {
-
+        String input;
+        while (true) {
+            input = sc.nextLine();
+            if (input.matches("[\\pL\\pMn*\\s*]+")) {
+                input = input.trim();
+                input = input.replaceAll("\\s+", " ");
+                input = input.toLowerCase();
+                String[] Chuoi = input.split(" ");
+                input = "";
+                for (int i = 0; i < Chuoi.length; i++) {
+                    input += String.valueOf(Chuoi[i].charAt(0)).toUpperCase() + Chuoi[i].substring(1);
+                    if (i < Chuoi.length - 1) {
+                        input += " ";
+                    }
+                }
+                return input;
+            } else {
+                System.out.print("Ten nha cung cap khong hop le.");
+            }
+            System.out.println("Moi nhap lai: ");
+        }
     }
 
-    public static int checkSoNguyen() {
-
+    public static int checkLuaChon() {
+        int input;
+        while (true) {
+            try {
+                input = Integer.parseInt(sc.nextLine());
+                return input;
+            } catch (NumberFormatException exception) {
+                return -1;
+            }
+        }
     }
 }
